a5540f7eff8b5681729a6c825d6d159839b0fd76
Florian <skatepainter@yahoo.de>
2016-07-03 22:14:56 +0200

    Started implementing the model classes.

--- src/main/java/com/floriantoenjes/blog/model/BlogEntry.java
+++ src/main/java/com/floriantoenjes/blog/model/BlogEntry.java
@@ -1,8 +1,28 @@
 package com.floriantoenjes.blog.model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class BlogEntry {
+    int id;
+    String author;
+    String content;
+    String title;
+    List<String> tags;
+    List<Comment> commentList;
+    LocalDateTime creationtime;
+
     public boolean addComment(Comment comment) {
         // Store these comments!
         return false;
+    }
+
+    public BlogEntry(int id, String author, String content, String title, List<String> tags) {
+        this.id = id;
+        this.author = author;
+        this.content = content;
+        this.title = title;
+        this.tags = tags;
+        creationtime = LocalDateTime.now();
     }
 }
--- src/main/java/com/floriantoenjes/blog/model/Comment.java
+++ src/main/java/com/floriantoenjes/blog/model/Comment.java
@@ -1,4 +1,12 @@
 package com.floriantoenjes.blog.model;
 
+import java.time.LocalDateTime;
+
 public class Comment {
+    int id;
+    String author;
+    String content;
+    LocalDateTime creationTime;
+
+
 }
