1749b9d87932868d96d8e62dad28996f4d7b9252
Florian <skatepainter@yahoo.de>
2016-07-04 19:35:17 +0200

    Added handlebars templating.

--- src/main/java/com/floriantoenjes/blog/Main.java
+++ src/main/java/com/floriantoenjes/blog/Main.java
@@ -1,16 +1,27 @@
 package com.floriantoenjes.blog;
 
+import com.floriantoenjes.blog.dao.BlogDao;
 import com.floriantoenjes.blog.dao.SimpleBlogDao;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.*;
 
 public class Main {
-    SimpleBlogDao dao;
+    static BlogDao dao;
 
     public static void main(String[] args) {
         staticFileLocation("/public");
+        dao = new SimpleBlogDao();
 
-        get("/", (req, res) -> "Hello World");
+        get("/", (req, res) -> {
+            Map<String, Object> modelMap = new HashMap<>();
+            modelMap.put("entries", dao.findAllEntries());
+            return new ModelAndView(modelMap, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 
 }
