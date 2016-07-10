c872da2c373c06c851f884cbf60649b0a7a9e006
Florian <skatepainter@yahoo.de>
2016-07-10 16:41:01 +0200

    Minor changes.

--- src/main/java/com/floriantoenjes/blog/Main.java
+++ src/main/java/com/floriantoenjes/blog/Main.java
@@ -17,27 +17,24 @@ import java.util.Map;
 import static spark.Spark.*;
 
 public class Main {
-    private static BlogDao dao;
-    private static HandlebarsTemplateEngine hbsEngine;
+    private static BlogDao dao = new SimpleBlogDao();
+    private static HandlebarsTemplateEngine hbsEngine = new HandlebarsTemplateEngine();
     private static Slugify slugify;
 
-    public static void main(String[] args) {
-        dao = new SimpleBlogDao();
-        hbsEngine = new HandlebarsTemplateEngine();
+    static {
         try {
             slugify = new Slugify();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public static void main(String[] args) {
         // Set location of static files
         staticFileLocation("/public");
 
-        // Mock blog data
-        String titleTmp = "A Great Day with a Friend";
-        String slugTmp = slugify.slugify(titleTmp);
-        dao.addEntry(new BlogEntry("Florian Antonius", titleTmp, slugTmp,
-                "It was an amazing day with a good friend.", Arrays.asList("Friends", "Amazing")));
+        // Create first blog entries
+        createMockData();
 
         // Cookie assignment to attribute
         before((req, res) -> {
@@ -46,14 +43,14 @@ public class Main {
             }
         });
 
-        // List all blog entries
+        // Listing of all blog entries
         get("/", (req, res) -> {
             Map<String, Object> modelMap = new HashMap<>();
             modelMap.put("entries", dao.findAllEntries());
             return new ModelAndView(modelMap, "index.hbs");
         }, hbsEngine);
 
-        // Create a new blog entry
+        // Creating a new blog entry
         before("/new.html", (req, res) -> {
             if (req.attribute("user") == null || !req.attribute("user").equals("admin")) {
                 res.redirect("/password.html");
@@ -61,7 +58,7 @@ public class Main {
             }
         });
 
-        // Submit the creation of a new blog entry
+        // Submitting the creation of a new blog entry
         post("/", (req, res) -> {
             String author = "Florian Antonius";
             String title = req.queryParams("title");
@@ -112,7 +109,7 @@ public class Main {
             return res;
         });
 
-        // Edit a blog entry
+        // Editing a blog entry
         get("/entry/:slug/edit", (req, res) -> {
             BlogEntry blogEntry = dao.findEntryBySlug(req.params(":slug"));
             Map<String, Object> modelMap = new HashMap<>();
@@ -146,5 +143,25 @@ public class Main {
             res.redirect("/password.html");
             return res;
         });
+    }
+
+    public static void createMockData() {
+        String titleTmp;
+        String slugTmp;
+
+        titleTmp = "A Great Day with a Friend";
+        slugTmp = slugify.slugify(titleTmp);
+        dao.addEntry(new BlogEntry("Florian Antonius", titleTmp, slugTmp,
+                "It was an amazing day with a good friend.", Arrays.asList("Friends", "Amazing")));
+
+        titleTmp = "The Unusual Coder";
+        slugTmp = slugify.slugify(titleTmp);
+        dao.addEntry(new BlogEntry("Florian Antonius", titleTmp, slugTmp,
+                "Some people code in extraordinarily strange ways.", Arrays.asList("People", "Coding", "Strange")));
+
+        titleTmp = "What Will This Day Bring?";
+        slugTmp = slugify.slugify(titleTmp);
+        dao.addEntry(new BlogEntry("Florian Antonius", titleTmp, slugTmp,
+                "Isn't it always a mystery what is going to happen next?", Arrays.asList("Time")));
     }
 }
